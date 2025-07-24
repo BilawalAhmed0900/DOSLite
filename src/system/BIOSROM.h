d60e5ffc711a968ea7a887cfb9ebb28596a6a5f0
Bilawal Ahmed <blwal7057@gmail.com>
2025-07-24 22:41:43 +0200

    small

--- src/system/BIOSROM.h
+++ src/system/BIOSROM.h
@@ -8,12 +8,15 @@
 class BIOSROM : public ROM {
  public:
   virtual ~BIOSROM() = default;
+  BIOSROM(const std::vector<uint8_t>& data);
 
+ public:
   /*
     Real system BIOS addresses in 8086, 65535 bytes
   */
   constexpr static size_t STARTING_ADDR = 0xF0000;
-  constexpr static size_t ENDING_ADDR   = 0x100000; // end exclusive
+  constexpr static size_t ENDING_ADDR = 0x100000;  // end exclusive
   constexpr static size_t MAX_REAL_SIZE = ENDING_ADDR - STARTING_ADDR - 1;
-  BIOSROM(const std::vector<uint8_t>& data);
+
+ private:
 };
